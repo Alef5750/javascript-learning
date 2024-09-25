87ec709094335db3c8f58de7cd411c38831be67e
Alef5750 <alewitt@gmail.com>
2024-09-25 09:27:44 +0300

    initial server code and other pkgs

--- NBA/script.js
+++ NBA/script.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,49 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var _this = this;
-var form = document.querySelector("#player-search");
-var position = document.querySelector("#position");
-var fgprecent = document.querySelector("#field-goals-precents");
-var threeprecent = document.querySelector("#three-precents");
-var points = document.querySelector("#points");
-var BASEURL = "https://nbaserver-q21u.onrender.com/api/";
+const form = document.querySelector("#player-search");
+const position = document.querySelector("#position");
+const fgprecent = document.querySelector("#field-goals-precents");
+const threeprecent = document.querySelector("#three-precents");
+const points = document.querySelector("#points");
+const BASEURL = "https://nbaserver-q21u.onrender.com/api/";
 var Positions;
 (function (Positions) {
     Positions[Positions["PG"] = 0] = "PG";
@@ -58,123 +22,95 @@ var Positions;
     Positions[Positions["PF"] = 3] = "PF";
     Positions[Positions["C"] = 4] = "C";
 })(Positions || (Positions = {}));
-var setElements = function (position) {
-    var elements = {
+const setElements = (position) => {
+    let elements = {
         maindiv: undefined,
         heading: undefined,
         detaildiv: undefined,
     };
-    elements.maindiv = document.getElementById("".concat(position, "div"));
+    elements.maindiv = document.getElementById(`${position}div`);
     elements.heading = elements.maindiv.querySelector("h3");
     elements.detaildiv = elements.maindiv.querySelector("div");
     return elements;
 };
-var SetPlayer = function (player, playerelements) {
+const SetPlayer = (player, playerelements) => {
     //cleaning
     playerelements.detaildiv.innerHTML = "";
     //starting to insert
     playerelements.heading.innerText = player.playerName || "";
-    var playerPoints = document.createElement('h4');
-    playerPoints.textContent = "Points : ".concat(player.points);
-    var playerTwo = document.createElement('h4');
-    playerTwo.textContent = "Two Precents : ".concat(player.twoPercent, "%");
-    var playerThree = document.createElement('h4');
-    playerThree.textContent = "Three Precents : ".concat(player.threePercent, "%");
+    const playerPoints = document.createElement('h4');
+    playerPoints.textContent = `Points : ${player.points}`;
+    const playerTwo = document.createElement('h4');
+    playerTwo.textContent = `Two Precents : ${player.twoPercent}%`;
+    const playerThree = document.createElement('h4');
+    playerThree.textContent = `Three Precents : ${player.threePercent}%`;
     playerelements.detaildiv.appendChild(playerThree);
     playerelements.detaildiv.appendChild(playerTwo);
     playerelements.detaildiv.appendChild(playerPoints);
 };
-var team = [
+const team = [
     setElements("PG"),
     setElements("SG"),
     setElements("SF"),
     setElements("PF"),
     setElements("C"),
 ];
-var postData = function () {
-    var args_1 = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args_1[_i] = arguments[_i];
-    }
-    return __awaiter(_this, __spreadArray([], args_1, true), void 0, function (url, data) {
-        var response, message, responseData, error_1;
-        if (url === void 0) { url = ""; }
-        if (data === void 0) { data = {}; }
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch(url, {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify(data),
-                        })];
-                case 1:
-                    response = _a.sent();
-                    // Check if the response was successful
-                    if (!response.ok) {
-                        message = "An error occurred: ".concat(response.statusText);
-                        throw new Error(message);
-                    }
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    responseData = _a.sent();
-                    return [2 /*return*/, responseData]; // Return the data from the response
-                case 3:
-                    error_1 = _a.sent();
-                    console.error("Error:", error_1);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
+const postData = (...args_1) => __awaiter(void 0, [...args_1], void 0, function* (url = "", data = {}) {
+    try {
+        const response = yield fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
         });
-    });
-};
-form.addEventListener("submit", function (e) { return __awaiter(_this, void 0, void 0, function () {
-    var filterPlayer, response;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                e.preventDefault();
-                console.log("started filterring");
-                filterPlayer = {
-                    position: position.value,
-                    threePercent: threeprecent.valueAsNumber,
-                    twoPercent: fgprecent.valueAsNumber,
-                    points: points.valueAsNumber,
-                };
-                return [4 /*yield*/, postData(BASEURL + "filter", filterPlayer)];
-            case 1:
-                response = _a.sent();
-                updateTable(response);
-                return [2 /*return*/];
+        // Check if the response was successful
+        if (!response.ok) {
+            const message = `An error occurred: ${response.statusText}`;
+            throw new Error(message);
         }
-    });
-}); });
+        const responseData = yield response.json(); // Parse JSON data from the response
+        return responseData; // Return the data from the response
+    }
+    catch (error) {
+        console.error("Error:", error);
+    }
+});
+form.addEventListener("submit", (e) => __awaiter(void 0, void 0, void 0, function* () {
+    e.preventDefault();
+    console.log("started filterring");
+    const filterPlayer = {
+        position: position.value,
+        threePercent: threeprecent.valueAsNumber,
+        twoPercent: fgprecent.valueAsNumber,
+        points: points.valueAsNumber,
+    };
+    const response = yield postData(BASEURL + "filter", filterPlayer);
+    updateTable(response);
+}));
 function updateTable(players) {
     // Clear existing table entries
-    var tbody = document.querySelector("#main-search-tbody");
+    const tbody = document.querySelector("#main-search-tbody");
     tbody.innerHTML = "";
     // Generate new rows for each player and append to the table
-    players.forEach(function (player) {
+    players.forEach((player) => {
         var _a;
-        var row = document.createElement("tr");
-        var playerCell = document.createElement("td");
+        const row = document.createElement("tr");
+        const playerCell = document.createElement("td");
         playerCell.textContent = player.playerName || "";
-        var positionCell = document.createElement("td");
+        const positionCell = document.createElement("td");
         positionCell.textContent = player.position;
-        var pointsCell = document.createElement("td");
-        pointsCell.textContent = "".concat(player.points);
-        var fgPercentCell = document.createElement("td");
-        fgPercentCell.textContent = "".concat(player.twoPercent);
-        var threePercentCell = document.createElement("td");
-        threePercentCell.textContent = "".concat(player.threePercent);
-        var ActionCell = document.createElement("td");
-        var Addbutton = document.createElement("button");
+        const pointsCell = document.createElement("td");
+        pointsCell.textContent = `${player.points}`;
+        const fgPercentCell = document.createElement("td");
+        fgPercentCell.textContent = `${player.twoPercent}`;
+        const threePercentCell = document.createElement("td");
+        threePercentCell.textContent = `${player.threePercent}`;
+        const ActionCell = document.createElement("td");
+        const Addbutton = document.createElement("button");
         Addbutton.id = "AddButton";
-        Addbutton.textContent = "Add ".concat((_a = player.playerName) === null || _a === void 0 ? void 0 : _a.split(" ")[0], " to Current Team");
-        Addbutton.addEventListener("click", function (e) { SetPlayer(player, team[Positions[player.position]]); });
+        Addbutton.textContent = `Add ${(_a = player.playerName) === null || _a === void 0 ? void 0 : _a.split(" ")[0]} to Current Team`;
+        Addbutton.addEventListener("click", (e) => { SetPlayer(player, team[Positions[player.position]]); });
         ActionCell.appendChild(Addbutton);
         // Append cells to the row
         row.appendChild(playerCell);
--- todo-list/index.js
+++ todo-list/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,17 +10,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const baseUrl = "https://66e9905787e417609449f8bc.mockapi.io/api/v1";
 // GET ALL TODO'S
-const getAll = () => __awaiter(this, void 0, void 0, function* () {
+const getAll = () => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield fetch(`${baseUrl}/todos`);
     const data = yield response.json();
     console.log(data);
 });
 // CREATE NEW TODO
-const addTodo = (newTodo) => __awaiter(this, void 0, void 0, function* () {
+const addTodo = (newTodo) => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield fetch(`${baseUrl}/todos`, {
         method: "POST",
         body: JSON.stringify(newTodo)
     });
 });
 getAll();
-//# sourceMappingURL=index.js.map
