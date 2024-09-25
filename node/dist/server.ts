02c35fe4c27492329e3ccfc13cd09989ee66a393
Alef5750 <alewitt@gmail.com>
2024-09-25 10:13:09 +0300

    created user interface

--- node/dist/server.ts
+++ node/dist/server.ts
@@ -1,14 +1,25 @@
 // Import the 'express' module
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { v4 as uuid } from "uuid";
+import bcrypt from "bcrypt";
 
+interface User {
+  id: string, 
+  email: string,
+  password: string
+}
 // Create an Express application
 const app = express();
 
 // Set the port number for the server
 const port = 3000;
 
+// make a get request for getting all users
+app.get('/users', (req: Request, res: Response) => {
+
+})
 // Define a route for the root path ('/')
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   // Send a response to the client
   res.send('Hello, TypeScript + Node.js + Express!');
 });
