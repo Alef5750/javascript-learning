2fe890c818e06fffb30c9c7b55175672af10ca78
Alef5750 <alewitt@gmail.com>
2024-09-25 12:15:08 +0300

    basic client side

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Serve static files from a 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Create HTTP server by passing the Express app
+const server = http.createServer(app);
 
 app.listen(PORT, () => {
   console.log(`EXPRESS Server running on port ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from EXPRESS server!");
-});
+// NOT RELEVANT SINCE SERVING ONLY STATIC FILES
+// app.get("/", (req, res) => {
+//   res.send("Hello from EXPRESS server!");
+// });
 
-// Create a websocket server and run it on port 8181
-const wsServer = new WebSocket.Server({ port: 8181 });
+// Integrate  WebSocket with the HTTP server (single port)
+const wsServer = new WebSocket.Server({ server });
 
 // listen for new websocket connections
 wsServer.on("connection", function connection(ws) {
@@ -21,7 +30,9 @@ wsServer.on("connection", function connection(ws) {
   // listen for messages sent by client
   ws.on("message", function incoming(message) {
     console.log(message.toString());
+    //send the message back to client
+    ws.send(`Echo: ${message}`);
   });
 
-  //   ws.send("This is a message, אחלה");
+  ws.send("This is a message, אחלה");
 });
